e70f982cef5da0be61dd26e6a0d721a9195edfcb
Lea Edwards <leaxedw@gmail.com>
2026-05-11 21:27:35 -0500

    Cart deletion and window confirmations

--- pages/cart.js
+++ pages/cart.js
@@ -7,7 +7,7 @@ import CartDetail from "../components/order/detail";
 import CompleteFormModal from "../components/order/form-modal";
 import { completeCurrentOrder, getCart } from "../data/orders";
 import { getPaymentTypes } from "../data/payment-types";
-import { removeProductFromOrder } from "../data/products";
+import { deleteCart, removeProductFromOrder } from "../data/products";
 
 export default function Cart() {
   const [cart, setCart] = useState({});
@@ -40,6 +40,18 @@ export default function Cart() {
     );
   };
 
+  const deleteOrder = () => {
+    if (
+      window.confirm(
+        "Are you sure you want to delete all the items in your cart?",
+      )
+    ) {
+      deleteCart().then(() => {
+        setCart({});
+      });
+    }
+  };
+
   const removeProduct = (productId) => {
     removeProductFromOrder(productId).then(refresh);
   };
@@ -61,7 +73,9 @@ export default function Cart() {
           >
             Complete Order
           </a>
-          <a className="card-footer-item">Delete Order</a>
+          <a className="card-footer-item" onClick={deleteOrder}>
+            Delete Order
+          </a>
         </>
       </CardLayout>
     </>
