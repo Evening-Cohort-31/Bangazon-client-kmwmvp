90b3fc569b2334988a5e0448fc1858a1e115fa27
James Freeman <jamesfreeman114@gmail.com>
2026-06-13 11:41:05 -0500

    Removed reviews from rating form

--- components/rating/card.js
+++ components/rating/card.js
@@ -7,13 +7,6 @@ export function RatingCard({ rating }) {
         <figure className="media-left">
           <Rating initialValue={rating} readonly={true} />
         </figure>
-        {/* <div className="media-content">
-          <div className="content">
-            <p>
-              {rating.review}
-            </p>
-          </div> */}
-        {/* </div> */}
       </article>
     </div>
   )
